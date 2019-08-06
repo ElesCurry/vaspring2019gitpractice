1bece3883623fbdfe029ab28e3f0212ca17d157e
elescurry <elesamcb@gmail.com>
2019-08-06 19:29:01 -0400

    adding second file

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,5 +2,9 @@ public class AppTest {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
     }
 }
